379f044a52c0c4ebf3c15495e13cbd9112dc84f7
higzo <ashrafhgazy9@gmail.com>
2025-10-27 19:18:44 +0300

    Initial

---
+++ Session 1/Infrastructure/PersistenceLayer/DataSeeding.cs
@@ -0,0 +1,16 @@
+﻿using DomanLayer.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersistenceLayer
+{
+    internal class DataSeeding : IDataSeeding
+    {
+        public void DataSeed()
+        {
+        }
+    }
+}
